93db1c377ed73175d40fd346ca409813f42ee111
lswlover2579 <354058870@qq.com>
2020-12-24 10:48:28 +0800

    Update Treasure_Box_3.py

--- scripts/Treasure_Box_3.py
+++ scripts/Treasure_Box_3.py
@@ -217,7 +217,7 @@ def qq_read():
                 print(f'treasurebox=={treasure_box_reward}')
                 if treasure_box_reward:
                     content += f"\n【开启第{treasure_box_reward['count']}个宝箱】获得{treasure_box_reward['amount']}金币"
-                    bark_content += f"\n【开启第{treasure_box_reward['count']}个宝箱】获得{treasure_box_reward['amount']}金币"
+                    #bark_content += f"\n【开启第{treasure_box_reward['count']}个宝箱】获得{treasure_box_reward['amount']}金币"
                     #notify.send(mark='t',title=title, content=content, notify_mode=notify_mode)
             
             # 宝箱金币奖励翻倍
@@ -232,12 +232,12 @@ def qq_read():
                 if treasure_box_ads_reward:
                     content += f"\n【宝箱奖励翻倍】获得{treasure_box_ads_reward['amount']}金币"
                 if balance > 10:
-                	bark_content += str(balance)
+                	bark_content += '恭喜获得十元现金红包\n前去QQ领钱💰'
                 	notify.send(mark='b',title=bark_title, content=bark_content, notify_mode=notify_mode)
             else:
             	print('Time CD..in log')
             	if balance > 10:
-            		bark_content += str(balance)
+            		bark_content += '恭喜获得十元现金红包\n前去QQ领钱💰'
             		notify.send(mark='b',title=bark_title, content=bark_content, notify_mode=notify_mode)
             	time_content = 'Time CD...\n--T1'
             	#notify.send(mark='t',title=title, content= time_content, notify_mode=notify_mode)
